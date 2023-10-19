fb7b1d9f06e637b1c9833665a52072e1a93ff360
Sontara444 <sontararajput@gmail.com>
2023-10-20 00:32:51 +0530

    add images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+// const Button = styled.button`
+// background-color: black;
+// color: white;
+// padding: 10px;
+// `;
+const Button = styled.button`;`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+    hello
+       <Button> this is Style Components</Button>
+    </>
   );
 }
 
